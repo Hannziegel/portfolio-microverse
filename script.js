faac7d60733bcda76eaf8d15d859a93671a97b79
Rodrigo Johann Reckziegel Nunes <hannziegel@gmail.com>
2022-06-13 16:51:03 -0300

    Add script.js file and CSS menu rules

---
+++ script.js
@@ -0,0 +1,12 @@
+let 
+
+// Change the height of Header to cover all screen
+
+// Add hide class to burguer icon and remove hide class to close icon
+// close icon float right
+
+// Change display none to display flex in menu-links, add flex-direction column, add border-botton 
+
+// Add hide to logo
+
+// Add hide to message-icon
