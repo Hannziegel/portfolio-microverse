738e636729275f9be9102564902a038758b46836
Rodrigo Johann Reckziegel Nunes <hannziegel@gmail.com>
2022-06-14 08:42:07 -0300

    Fix transitions update script.js

--- script.js
+++ script.js
@@ -1,21 +1,14 @@
-document.querySelector("#mobMenu").addEventListener("click", () => 
-{
+document.querySelector("#mobMenu").addEventListener("click", () => {
+  // Change the height of Header to cover all screen
   document.querySelector(".collapse").classList.toggle("header-menu");
+  // Add hide class to burguer-icon, logo and remove for .close-icon
   document.querySelector(".logo").classList.toggle("hide");
   document.querySelector(".burguer-icon").classList.toggle("hide");
   document.querySelector(".close-icon").classList.toggle("hide");
+  // Change display none to display flex in menu-links, add flex-direction column, add border-botton
   document.querySelector(".right").classList.toggle("right-menu");
   document.querySelector(".menu-links").classList.toggle("menu-links-menu");
-  document.querySelectorAll(".menu-link").forEach(element => element.classList.toggle("menu-link-menu"));
+  document
+    .querySelectorAll(".menu-link")
+    .forEach((element) => element.classList.toggle("menu-link-menu"));
 });
-
-// Change the height of Header to cover all screen
-
-// Add hide class to burguer icon and remove hide class to close icon
-// close icon float right
-
-// Change display none to display flex in menu-links, add flex-direction column, add border-botton 
-
-// Add hide to logo
-
-// Add hide to message-icon
